4ce0169b0bdbb456e81a8482148249fde5f11534
danielBramer <bramerdaniel@gmx.de>
2017-07-07 12:24:20 +0200

    Made custom header nad footer public

--- ConsoLovers.ConsoleToolkit/Contracts/ICustomFooter.cs
+++ ConsoLovers.ConsoleToolkit/Contracts/ICustomFooter.cs
@@ -1,9 +1,17 @@
+// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ICustomFooter.cs" company="ConsoLovers">
+//    Copyright (c) ConsoLovers  2015 - 2017
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
 namespace ConsoLovers.ConsoleToolkit.Contracts
 {
-   internal interface ICustomFooter
+   /// <summary>Interface for customizing the menus footer</summary>
+   public interface ICustomFooter
    {
       #region Public Methods and Operators
 
+      /// <summary>Prints the footer of the menu.</summary>
       void PrintFooter();
 
       #endregion
--- ConsoLovers.ConsoleToolkit/Contracts/ICustomHeader.cs
+++ ConsoLovers.ConsoleToolkit/Contracts/ICustomHeader.cs
@@ -1,15 +1,17 @@
 // --------------------------------------------------------------------------------------------------------------------
 // <copyright file="ICustomHeader.cs" company="ConsoLovers">
-//   Copyright (c) ConsoLovers  2015 - 2016
+//    Copyright (c) ConsoLovers  2015 - 2017
 // </copyright>
 // --------------------------------------------------------------------------------------------------------------------
 
 namespace ConsoLovers.ConsoleToolkit.Contracts
 {
-   internal interface ICustomHeader
+   /// <summary>Interface for customizing the menus header.</summary>
+   public interface ICustomHeader
    {
       #region Public Methods and Operators
 
+      /// <summary>Prints the header of the menu.</summary>
       void PrintHeader();
 
       #endregion
