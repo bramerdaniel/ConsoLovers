8b6cd311f67b0d78e08f2ccde384ac2ab85ae927
Daniel Bramer <Daniel.Bramer@kuka.com>
2022-07-29 12:31:03 +0200

    Added input box to console toolkit core

---
+++ src/ConsoLovers.ConsoleToolkit.Core/Input/InputLabel.cs
@@ -0,0 +1,59 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="InputLabel.cs" company="KUKA Deutschland GmbH">
+//   Copyright (c) KUKA Deutschland GmbH 2006 - 2022
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace ConsoLovers.ConsoleToolkit.Core.Input;
+
+using System;
+using System.Diagnostics.CodeAnalysis;
+
+using ConsoLovers.ConsoleToolkit.Core;
+
+[SuppressMessage("ReSharper", "AutoPropertyCanBeMadeGetOnly.Global")]
+[SuppressMessage("ReSharper", "MemberCanBePrivate.Global")]
+public class InputLabel
+{
+   #region Constants and Fields
+
+   private readonly IConsole console;
+
+   #endregion
+
+   #region Constructors and Destructors
+
+   public InputLabel(string text)
+      : this(new ConsoleProxy(), text)
+   {
+   }
+
+   public InputLabel(IConsole console, string text)
+   {
+      Text = text;
+      this.console = console;
+      Foreground = console.ForegroundColor;
+      Background = console.BackgroundColor;
+   }
+
+   #endregion
+
+   #region Public Properties
+
+   public ConsoleColor Background { get; set; }
+
+   public ConsoleColor Foreground { get; set; }
+
+   public string Text { get; set; }
+
+   #endregion
+
+   #region Public Methods and Operators
+
+   public void Print()
+   {
+      console.Write(Text, Foreground, Background);
+   }
+
+   #endregion
+}
---
+++ src/ConsoLovers.ConsoleToolkit.Core/Input/InputRange.cs
@@ -0,0 +1,161 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="InputRange.cs" company="KUKA Deutschland GmbH">
+//   Copyright (c) KUKA Deutschland GmbH 2006 - 2022
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace ConsoLovers.ConsoleToolkit.Core.Input;
+
+using System;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+using JetBrains.Annotations;
+
+[DebuggerDisplay("{Text} [Position={Position}, Length={Length}]")]
+[SuppressMessage("ReSharper", "IntroduceOptionalParameters.Global")]
+internal class InputRange
+{
+   #region Constants and Fields
+
+   private readonly StringBuilder textBuilder;
+
+   #endregion
+
+   #region Constructors and Destructors
+
+   public InputRange([CanBeNull] string initialText)
+      : this(initialText, int.MaxValue)
+   {
+   }
+
+   public InputRange([CanBeNull] string initialText, int maximalLength)
+   {
+      textBuilder = new StringBuilder(initialText);
+      Position = initialText?.Length ?? 0;
+
+      if (maximalLength > 0)
+         MaximalLength = maximalLength;
+   }
+
+   public InputRange()
+      : this(string.Empty, int.MaxValue)
+   {
+   }
+
+   #endregion
+
+   #region Public Properties
+
+   public int Length => textBuilder.Length;
+
+   public int? MaximalLength { get; }
+
+   public InsertionMode Mode { get; set; }
+
+   public int Position { get; private set; }
+
+   public string Text => textBuilder.ToString();
+
+   #endregion
+
+   #region Public Methods and Operators
+
+   public bool End()
+   {
+      Position = Length;
+      return true;
+   }
+
+   public bool Home()
+   {
+      Position = 0;
+      return true;
+   }
+
+   public bool Insert(char character)
+   {
+      if (Mode == InsertionMode.Insert)
+      {
+         if (MaximalLength.HasValue && Length + 1 > MaximalLength)
+            return false;
+
+         textBuilder.Insert(Position, character);
+         Position++;
+         return true;
+      }
+
+      if (textBuilder.Length == Position)
+      {
+         if (MaximalLength.HasValue && Length + 1 > MaximalLength)
+            return false;
+
+         textBuilder.Append(character);
+         Position++;
+         return true;
+      }
+
+      textBuilder.Remove(Position, 1);
+      textBuilder.Insert(Position, character);
+      Position++;
+      return true;
+   }
+
+   public bool Move(int offset)
+   {
+      var newPosition = Position + offset;
+      if (newPosition < 0)
+      {
+         Position = 0;
+         return false;
+      }
+
+      if (MaximalLength.HasValue && newPosition > MaximalLength)
+         return false;
+
+      if (newPosition > textBuilder.Length)
+         return false;
+
+      Position += offset;
+      return true;
+   }
+
+   public bool Remove()
+   {
+      return RemoveInternal(1);
+   }
+
+   #endregion
+
+   #region Methods
+
+   private bool RemoveInternal(int length)
+   {
+      if (length < 0)
+         throw new ArgumentOutOfRangeException(nameof(length), "Length to delete must be greater than zero");
+
+      if (length == 0)
+         return false;
+
+      if (Mode == InsertionMode.Insert)
+      {
+         var positionAfterRemove = Position - length;
+         if (positionAfterRemove < 0)
+            return false;
+
+         Position = positionAfterRemove;
+         textBuilder.Remove(Position, length);
+
+         return true;
+      }
+
+      if (textBuilder.Length <= Position)
+         return false;
+
+      textBuilder.Remove(Position, length);
+      return true;
+   }
+
+   #endregion
+}
