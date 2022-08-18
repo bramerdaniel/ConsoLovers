51b2647e7827e68a52f35c80222318c902322657
Christian Deisenhofer <christian.deisenhofer@kuka.com>
2022-08-18 13:26:17 +0200

    fixed compiler error

--- src/ConsoLovers.ConsoleToolkit/InputHandler/ConsoleInputHandler.cs
+++ src/ConsoLovers.ConsoleToolkit/InputHandler/ConsoleInputHandler.cs
@@ -13,6 +13,8 @@ namespace ConsoLovers.ConsoleToolkit.InputHandler
    {
       private Thread thread;
 
+      private bool isRunning;
+
       #region Public Events
 
       public event EventHandler<KeyEventArgs> KeyDown;
@@ -33,69 +35,38 @@ namespace ConsoLovers.ConsoleToolkit.InputHandler
 
       public void Start()
       {
-         thread = new Thread(
-            () =>
-            {
-               while (true)
-               {
-                  uint numRead = 0;
-                  INPUT_RECORD[] record = new INPUT_RECORD[1];
-                  record[0] = new INPUT_RECORD();
-                  ReadConsoleInput(handleIn, record, 1, ref numRead);
-                  if (isRunning)
-                     switch (record[0].EventType)
-                     {
-                        case INPUT_RECORD.MOUSE_EVENT:
-                           var mouseEvent = record[0].MouseEvent;
-                           if (mouseEvent.dwButtonState == dwButtonStates.FROM_LEFT_1ST_BUTTON_PRESSED)
-                           {
-                              MouseClicked?.Invoke(this, CreateEventArgs(mouseEvent));
-                           }
-                           if (mouseEvent.dwEventFlags == dwEventFlags.DOUBLE_CLICK)
-                           {
-                              MouseDoubleClicked?.Invoke(this, CreateEventArgs(mouseEvent));
-                           }
-                           else if (mouseEvent.dwEventFlags == dwEventFlags.MOUSE_MOVED)
-                           {
-                              MouseMoved?.Invoke(this, CreateEventArgs(mouseEvent));
-                           }
-                           else if (mouseEvent.dwEventFlags == dwEventFlags.MOUSE_HWHEELED || mouseEvent.dwEventFlags == dwEventFlags.MOUSE_WHEELED)
-                           {
-                              MouseWheelChanged?.Invoke(this, CreateEventArgs(mouseEvent));
-                           }
+         ////thread = new Thread(
+         ////   () =>
+         ////   {
+         ////      while (true)
+         ////      {
+         ////         if (isRunning)
 
-                           break;
-                        case INPUT_RECORD.KEY_EVENT:
-                           var keyEvent = record[0].KeyEvent;
+         ////            var keyEvent = record[0].KeyEvent;
 
-                           if (keyEvent.bKeyDown)
-                              KeyDown?.Invoke(this, CreateEventArgs(keyEvent));
+         ////         if (keyEvent.bKeyDown)
+         ////            KeyDown?.Invoke(this, CreateEventArgs(keyEvent));
+         ////      }
+         ////         else
+         ////      {
+         ////         uint numWritten = 0;
+         ////         WriteConsoleInput(handleIn, record, 1, ref numWritten);
+         ////         return;
+         ////      }
+         ////   }
+         ////   });
 
-                           break;
-                        case INPUT_RECORD.WINDOW_BUFFER_SIZE_EVENT:
-                           WindowBufferSizeEvent?.Invoke(record[0].WindowBufferSizeEvent);
-                           break;
-                     }
-                  else
-                  {
-                     uint numWritten = 0;
-                     WriteConsoleInput(handleIn, record, 1, ref numWritten);
-                     return;
-                  }
-               }
-            });
-
-         thread.Start();
+         ////thread.Start();
       }
 
-      public void Stop()
-      {
-      }
-
-      public void Wait()
-      {
-      }
-
-      #endregion
+   public void Stop()
+   {
    }
+
+   public void Wait()
+   {
+   }
+
+   #endregion
+}
 }
