5f0334d6ca6dc0449c198926403f0d87bec8aaab
Daniel Bramer <Daniel.Bramer@kuka.com>
2022-08-20 11:49:43 +0200

    Removed console application manager

--- src/Core/ConsoLovers.ConsoleToolkit.Core.UnitTests/TestHelperExtensions.cs
+++ src/Core/ConsoLovers.ConsoleToolkit.Core.UnitTests/TestHelperExtensions.cs
@@ -1,5 +1,5 @@
 ﻿// --------------------------------------------------------------------------------------------------------------------
-// <copyright file="HelperExtensions.cs" company="ConsoLovers">
+// <copyright file="TestHelperExtensions.cs" company="ConsoLovers">
 //    Copyright (c) ConsoLovers  2015 - 2022
 // </copyright>
 // --------------------------------------------------------------------------------------------------------------------
@@ -7,25 +7,12 @@
 namespace ConsoLovers.ConsoleToolkit.Core.UnitTests;
 
 using System;
-using ConsoLovers.ConsoleToolkit.Core.Bootstrappers;
 using ConsoLovers.ConsoleToolkit.Core.Builders;
 
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-public static class HelperExtensions
+public static class TestHelperExtensions
 {
-   internal static T BuildApplication<T>(this IBootstrapper<T> bootstrapper)
-      where T : class, IApplication
-   {
-      if (bootstrapper is GenericBootstrapper<T> genericBuilder)
-      {
-         var applicationManager = genericBuilder.CreateApplicationManager();
-         return applicationManager.CreateApplication();
-      }
-
-      throw new AssertFailedException($"The builder was not a {nameof(GenericBootstrapper<T>)}");
-   }
-
    internal static IServiceProvider CreateServiceProvider<T>(this IApplicationBuilder<T> builder)
       where T : class
    {
