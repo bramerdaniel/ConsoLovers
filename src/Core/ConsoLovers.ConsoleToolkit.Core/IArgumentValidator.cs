a8d1305cb6aa3426f594edd03381a634a7e3949c
Daniel Bramer <Daniel.Bramer@kuka.com>
2022-09-02 11:20:04 +0200

    added todos

--- src/Core/ConsoLovers.ConsoleToolkit.Core/IArgumentValidator.cs
+++ src/Core/ConsoLovers.ConsoleToolkit.Core/IArgumentValidator.cs
@@ -6,6 +6,9 @@
 
 namespace ConsoLovers.ConsoleToolkit.Core
 {
+   // TODO Add more build in validators NotNullOrEmpty, IsInRange...
+   // TODO Support Attribute derived types e.g. [NotNullOrWhitespace] as attribute
+
    /// <summary>Interface for implementing custom command line argument validation</summary>
    /// <typeparam name="T">The type of the value that should be validated</typeparam>
    public interface IArgumentValidator<in T>
