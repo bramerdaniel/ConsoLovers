ff8fe1a8dafc7bc6864fcb40a2ca1aa016024e43
Daniel Bramer <Daniel.Bramer@kuka.com>
2022-09-19 08:19:05 +0200

    Build fix

--- src/Core/ConsoLovers.ConsoleToolkit.Core/Services/DefaultExitCodeHandler.cs
+++ src/Core/ConsoLovers.ConsoleToolkit.Core/Services/DefaultExitCodeHandler.cs
@@ -6,9 +6,15 @@
 
 namespace ConsoLovers.ConsoleToolkit.Core.Services
 {
+   using System;
+
    /// <summary>Default implementation of the <see cref="IExitCodeHandler"/> interface</summary>
    /// <seealso cref="ConsoLovers.ConsoleToolkit.Core.IExitCodeHandler"/>
    internal sealed class DefaultExitCodeHandler : ExitCodeHandler
    {
+      protected override int ComputeExitCodeForOtherExceptions(Exception exception)
+      {
+         return 1;
+      }
    }
 }
