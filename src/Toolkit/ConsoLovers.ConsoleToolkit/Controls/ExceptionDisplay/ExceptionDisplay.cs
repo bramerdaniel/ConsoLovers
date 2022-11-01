15edf5cc5c93f0b82c7da7e7fd28aee520ba1af7
Daniel Bramer <Daniel.Bramer@kuka.com>
2022-11-01 23:24:28 +0100

    Refactoring

--- src/Toolkit/ConsoLovers.ConsoleToolkit/Controls/ExceptionDisplay/ExceptionDisplay.cs
+++ src/Toolkit/ConsoLovers.ConsoleToolkit/Controls/ExceptionDisplay/ExceptionDisplay.cs
@@ -7,11 +7,8 @@
 namespace ConsoLovers.ConsoleToolkit.Controls;
 
 using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.IO;
-using System.Reflection;
 
 using JetBrains.Annotations;
 
@@ -25,16 +22,14 @@ public class ExceptionDisplay : InteractiveRenderable
 
    public ExceptionDisplay([NotNull] Exception exception)
    {
+      // TODO provide some options for customization
       Exception = exception ?? throw new ArgumentNullException(nameof(exception));
-      ExceptionData = new ExceptionData(exception);
 
-      messageDisplay = new MessageDisplay(ExceptionData.MessageHint, exception.Message);
+      messageDisplay = new MessageDisplay($"{exception.GetType().Name}: ", exception.Message);
       
       StackTrace = new StackTrace(exception, fNeedFileInfo: true);
       stackTraceDisplay = new StackTraceDisplay(StackTrace);
    }
-
-
 
    #endregion
 
@@ -45,8 +40,7 @@ public class ExceptionDisplay : InteractiveRenderable
    #endregion
 
    #region Properties
-
-   private ExceptionData ExceptionData { get; set; }
+   
 
    private StackTrace StackTrace { get; set; }
 
@@ -77,51 +71,10 @@ public class ExceptionDisplay : InteractiveRenderable
       {
          foreach (var segment in stackTraceDisplay.RenderLine(context, line - messageDisplay.Size.Height))
             yield return segment;
-            
-
-         //var frame = ExceptionData.StackTrace.GetFrame(line - ExceptionData.MessageLines.Count);
-         //foreach (var segment in RenderStackFrame(frame))
-         //   yield return segment;
-
-         // yield return new Segment(this, frame.GetMethod().Name, RenderingStyle.Default.WithForeground(ConsoleColor.Red));
       }
    }
 
-   private IEnumerable<Segment> RenderStackFrame(StackFrame frame)
-   {
-      var methodBase = frame.GetMethod();
-      var irelevant = Style.WithForeground(ConsoleColor.Gray);
-
-      yield return new Segment(this, ExceptionData.StackFrameIndent, irelevant);
-      yield return new Segment(this, "at ", irelevant);
-      if (methodBase is MethodInfo methodInfo)
-         yield return new Segment(this, $"{methodInfo.ReturnType.AliasOrName()} ", Style.WithForeground(ConsoleColor.DarkCyan));
-      if (methodBase is ConstructorInfo ctorInfo)
-         yield return new Segment(this, $"{ctorInfo.DeclaringType.AliasOrName()}", Style.WithForeground(ConsoleColor.DarkCyan));
-      
-      yield return new Segment(this, $"{methodBase.Name}(", Style);
-
-      var parameters = methodBase.GetParameters();
-      for (var i = 0; i < parameters.Length; i++)
-      {
-         var parameter = parameters[i];
-         yield return new Segment(this, parameter.ParameterType.AliasOrName(), Style.WithForeground(ConsoleColor.DarkCyan));
-         if (i != parameters.Length - 1)
-            yield return new Segment(this, ", ", irelevant);
-      }
-
-      yield return new Segment(this, $")", Style);
-      yield return new Segment(this, " in ", irelevant);
-
-
-
-      var name = frame.GetFileName() ?? "NoFile.cs";
-      name = Path.GetFileName(name);
-      yield return new Segment(this, name, Style.WithForeground(ConsoleColor.DarkYellow));
-      yield return new Segment(this, ":", irelevant);
-      yield return new Segment(this, frame.GetFileLineNumber().ToString(), Style.WithForeground(ConsoleColor.Blue));
-   }
-
+ 
    public RenderingStyle MessageStyle { get; set; } = DefaultStyles.ErrorMessageStyle;
 
    #endregion
